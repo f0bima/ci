38b6878725d65aa257c14d8c98f5655078ed1be6
f0bima <34135338+f0bima@users.noreply.github.com>
2019-09-28 14:06:05 +0700

    Update cekcek.java

--- cekcek.java
+++ cekcek.java
@@ -4,28 +4,23 @@ public class Super_Duper{
 
      public static void main(String []args){
         Scanner input = new Scanner(System.in);
-        int n = 0;
-        int energi = 0;
-        int[] x = new int[1000];
+        int n=0;
+        int energi=0;
+        int[] x =  new int[100];
         int jumlahenergi = 0;
-        String text;
         
-
-        do {
-        	System.out.println("Masukan jumlah energi : ");
-        	n = input.nextInt();
-        } while (n<2 || n>1000);
-
+        
+        System.out.println("Masukan jumlah energi : ");
+        n = input.nextInt();
         energi = 2*n-1;
         System.out.println("Masukan energi : ");
-        input.nextLine();
-        text = input.nextLine();        
-
-	String[] xx = text.split(" ");
+        String text = input.nextLine();
+        // String text = "-10 20 30";
+        String[] xx = text.split("\\s+");
         
-        for (int i = 0; i < energi; i++) {
-		x[i] = Integer.parseInt(xx[i]);
-        }      
+        for (int i = 0; i < energi-1; i++) {
+            x[i] = Integer.parseInt(xx[i]);
+        }
         
         
         for (int i = 0; i < energi-1; i++) {
@@ -45,6 +40,5 @@ public class Super_Duper{
         jumlahenergi = jumlahenergi + x[energi-1];
         
         System.out.println(jumlahenergi);
-        
      }
 }
